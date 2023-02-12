0cea0dd985ababd8beb2c469aa2e3cd95a7b3509
unknown <fllamas360@protonmail.com>
2023-02-11 22:37:20 -0800

    Employee Salary data plus typo correction

---
+++ SQLQuery1_EmployeeSalary_Data.sql
@@ -0,0 +1,16 @@
+INSERT INTO EmployeeSalary VALUES
+(9101, 'Actress', 10000000),
+(9102, 'Actor/Dancer', 25000000),
+(9103, 'Actor', 12000000),
+(9104, 'Actress/Model/Singer', 165000000),
+(9105, 'Actor/Singer', 80000000),
+(9106, 'Actress/Singer/Filmaker', 25000000),
+(9107, 'Actor/Wrestler', 16000000),
+(9108, 'Actress/Singer', 40000000),
+(9109, 'Actor/File Producer/ Screenwriter', 300000000),
+(9110, 'Actress/Model', 6000000),
+(9111, 'Actor', 80000000),
+(9112, 'Actress', 11000000),
+(9113, 'Actor', 130000000),
+(9114, 'Actor/Screenwriter', 12000000),
+(9115, 'Actress', 5000000)
---
+++ SQLQuery6.sql
@@ -0,0 +1,2 @@
+Update EmployeeSalary
+Set JobRole = 'Actor/Film Producer/Screen Writer' WHERE EmployeeID = 9109;
